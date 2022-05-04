14ca1f5c58134cf64d32ba30b926e0c7b27ee86c
Waseem <waseem.riaz.9999@gmail.com>
2022-05-04 19:12:15 +0530

    added "\n" for next line

--- instructions.py
+++ instructions.py
@@ -1,10 +1,12 @@
+#Intro to CrossTalks for easier understanding of its usage
+
 class instructs:
 	def welcome(user):
 		print("Hello" + user + "!!\n")
 		s = '''Welcome to CrossTalks.
 CrossTalks is a Terminal UI based full-duplex communicator.User messages
 will be clearly indicated to avoid confusion. Please type your message right
-":> " and press enter.'''
+":> " and press enter.\n'''
 		print(s)
 
 
