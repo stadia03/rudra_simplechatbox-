52bf23d2b9e33194b38ca2d479df210a6f137dd2
Gaurab Das <gaurab2003das@gmail.com>
2022-05-05 00:31:49 +0530

    meet server class

--- server.py
+++ server.py
@@ -1,16 +1,21 @@
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
 import sys
-PORT_NUMBER = 5000
-SIZE = 1024
+class Server:
+	def __init__(self, port, size):
+		self.port = port
+		self.size = size
+		self.host = gethostbyname('0.0.0.0')
+	def start(self):
+		self.socket = socket(AF_INET, SOCK_DGRAM)
+		self.socket.bind( (self.host, self.port) )
+		print("Sending server started on port {self.port}")
+	def recv(self):
+		(data, addr) = self.socket.recvfrom(self.size)
+		return data
 
-hostName = gethostbyname( '0.0.0.0' )
 
-mySocket = socket( AF_INET, SOCK_DGRAM )
-mySocket.bind( (hostName, PORT_NUMBER) )
-
-print ("Test server listening on port {0}\n".format(PORT_NUMBER))
-
-while True:
-        (data,addr) = mySocket.recvfrom(SIZE)
-        print(data)
-sys.ext()
+if __name__ == "__main__":
+	srv = Server(5000, 1024)
+	srv.start()
+	while True:
+		print(srv.recv())
