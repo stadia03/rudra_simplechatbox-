c61c51a230724223ed12e6cef7922201885225ea
Waseem <waseem.riaz.9999@gmail.com>
2022-05-04 19:09:55 +0530

    A simple introduction to the communicator for easier understanding of usage

---
+++ instructions.py
@@ -0,0 +1,10 @@
+class instructs:
+	def welcome(user):
+		print("Hello" + user + "!!\n")
+		s = '''Welcome to CrossTalks.
+CrossTalks is a Terminal UI based full-duplex communicator.User messages
+will be clearly indicated to avoid confusion. Please type your message right
+":> " and press enter.'''
+		print(s)
+
+
