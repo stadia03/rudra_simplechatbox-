2a9ce9e1f557e095e9be3435b114d275627447da
Shashwat Ganesh <shashwatganesh@protonmail.com>
2022-05-07 10:54:29 +0530

    Fix usage of Friend

--- sender.py
+++ sender.py
@@ -1,26 +1,27 @@
-#Trying to build a class which prints recieved message and get messages 
+# Trying to build a class which prints recieved message and get messages
 # from the user
 
+
 class UI:
-	def __init__(self, name, pc):
-		self.name = name
-		self.pc = pc
-	
-	#method to get message from the user
-	def get_message(self):
-		return input(self.name + "@" + self.pc + ":> ")
+    def __init__(self, name, pc):
+        self.name = name
+        self.pc = pc
 
-	#method to print the recieved message to the terminal
-	def print_message(string, friend):
-		print(sender.name + ":> "+string)
-	
-	def chooseFriend(friendlist):
-		for (i,f) in enumerate(friendlist):
-			print(f'Enter {i} for {f}')
-		
-		i = int(input())
-		while i not in range(len(friendlist)):
-			print('No such friend.Try again')
-			i = int(input())
+    # method to get message from the user
+    def get_message(self):
+        return input(self.name + "@" + self.pc + ":> ")
 
-		return i
+    # method to print the recieved message to the terminal
+    def print_message(string, friend):
+        print(friend.name + ":> " + string)
+
+    def chooseFriend(friendlist):
+        for (i, f) in enumerate(friendlist):
+            print(f"Enter {i} for {f}")
+
+        i = int(input())
+        while i not in range(len(friendlist)):
+            print("No such friend.Try again")
+            i = int(input())
+
+        return i
